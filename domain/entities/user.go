5befe2d107486b9897aa84e5d9476512f9660d4e
Thanawat Potidet <thanawatpotidech@gmail.com>
2024-09-30 13:45:55 +0700

    CREATE: Entity User

---
+++ domain/entities/user.go
@@ -0,0 +1,11 @@
+package entities
+
+type User struct {
+	UserId      string
+	Email       string
+	Fname       string
+	Lname       string
+	Password    string
+	PhoneNumber string
+	Address     string
+}
